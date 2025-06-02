6ea81199ea27bf5bba5be58354db635bfaecae85
minhduczzzz <duclmhe182150@fpt.edu.vn>
2025-06-02 22:18:34 +0700

    ordersucess

--- SWP391/src/java/dao/OrderDAO.java
+++ SWP391/src/java/dao/OrderDAO.java
@@ -3,21 +3,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dao;
+
 import config.ConnectDB;
+import java.math.BigDecimal;
 import model.*;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
 /**
  *
  * @author Window 11
  */
-public class OrderDAO extends ConnectDB{
-    public List<OrderDetail> orderlist(){ 
-        List<OrderDetail> list=new ArrayList<>();
-    return list;
-}
-    
-}
+public class OrderDAO extends ConnectDB {
+
+   
+    }
+
+
