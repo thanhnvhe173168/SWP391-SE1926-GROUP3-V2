57e05bb45db51548fa05656f50aa749d1060a2c1
thanhnv <thanhnvhe173168@fpt.edu.vn>
2025-07-25 01:19:59 +0700

    update

--- SWP391/src/java/controller/Guest/BlogList.java
+++ SWP391/src/java/controller/Guest/BlogList.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller.Guest;
 
 import dao.BlogDAO;
@@ -19,26 +18,29 @@ import java.sql.ResultSet;
  *
  * @author Admin
  */
-@WebServlet(name="BlogList", urlPatterns={"/blogList"})
+@WebServlet(name = "BlogList", urlPatterns = {"/blogList"})
 public class BlogList extends HttpServlet {
-    
+
     private static int PAGE_SIZE = 9;
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         BlogDAO blogDao = new BlogDAO();
 
         int currentPage = request.getParameter("currentPage") != null
                 ? Integer.parseInt(request.getParameter("currentPage"))
                 : 1;
-        ResultSet rsBlog = blogDao.getListBlog(currentPage, PAGE_SIZE, null, "active");
+        String title = request.getParameter("title");
+        ResultSet rsBlog = blogDao.getListBlog(currentPage, PAGE_SIZE, title, "active");
         int totalPage = 0;
         int totalRecord = blogDao.getTotalRecord("Select * from Blog");
         if (totalRecord % PAGE_SIZE != 0) {
@@ -51,11 +53,12 @@ public class BlogList extends HttpServlet {
         request.setAttribute("totalPage", totalPage);
         request.setAttribute("rsBlog", rsBlog);
         request.getRequestDispatcher("/guest/BlogList.jsp").forward(request, response);
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -63,12 +66,13 @@ public class BlogList extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -76,12 +80,13 @@ public class BlogList extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
